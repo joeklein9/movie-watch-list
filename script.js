e98b2f50a2745c17f3043186e32f4e98e8c5280b
Joseph Kleinhenz <115609619+joeklein9@users.noreply.github.com>
2023-02-14 14:47:21 -0500

    rendering top 10 results works

--- script.js
+++ script.js
@@ -5,14 +5,16 @@ const searchButton = document.getElementById("search-button")
 const resultsContainer = document.getElementById("results-container")
 
 searchButton.addEventListener("click", function () {
-    fetch (`http://www.omdbapi.com/?s=${searchBar.value}&apikey=ec55d5fb`)
-    .then(res => res.json())
-    .then(data => {
-        console.log(data.Search[0])
-        movieArray.push(data.Search[0])
-        renderResults()
-        
-    })
+    fetch(`http://www.omdbapi.com/?s=${searchBar.value}&apikey=ec55d5fb`)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data.Search)
+            for (let i = 0; i < data.Search.length; i++) {
+                movieArray.push(data.Search[i])
+            }
+            renderResults()
+
+        })
 })
 
 
@@ -21,8 +23,12 @@ function renderResults() {
     for (let movie of movieArray) {
         html += `
             <h3>${movie.Title}</h3>
+            <p>${movie.Year}</p>
+            <img src="${movie.Poster}">
             <hr />
         `
     }
+    document.getElementById("film-icon").style.visibility = "hidden"
+    document.getElementById("icon-subtext").style.visibility = "hidden"
     document.getElementById("results-container").innerHTML = html
 }
