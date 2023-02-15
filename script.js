f43ee2373e0bea948497fc0febde50dbd3403865
Joseph Kleinhenz <115609619+joeklein9@users.noreply.github.com>
2023-02-14 19:51:21 -0500

    refactoring

--- script.js
+++ script.js
@@ -2,35 +2,63 @@
 movieArray = []
 const searchBar = document.getElementById("search-bar")
 const searchButton = document.getElementById("search-button")
-const resultsContainer = document.getElementById("results-container")
+const posterContainer = document.getElementById("poster-container")
 
 searchButton.addEventListener("click", function () {
     fetch(`https://www.omdbapi.com/?s=${searchBar.value}&apikey=ec55d5fb`)
         .then(res => res.json())
         .then(data => {
+            
             console.log(data.Search)
+
+            
             for (let i = 0; i < data.Search.length; i++) {
                 movieArray.push(data.Search[i])
             }
-            renderResults()
+        
+            renderPoster()
+            renderDetails()
+            clearPlaceholders()
+            movieArray = []
+
 
         })
 })
 
 
-function renderResults() {
-    let html = ""
+function renderPoster() {
+    let imageHtml = ""
     for (let movie of movieArray) {
-        html += `
-            <h3>${movie.Title}</h3>
-            <p>${movie.Year}</p>
-            <img src="${movie.Poster}">
+
+        imageHtml += `
+            
+            <img src="${movie.Poster}" class = "poster-images">
             <hr />
         `
+        
     }
+    
+    document.getElementById("poster-container").innerHTML = imageHtml
+
+}
+
+
+function renderDetails () {
+    let detailsHtml = ""
+    for (let movie of movieArray) {
+
+        detailsHtml += `
+            
+            <p>${movie.Title}</p
+        `
+        
+    }
+    
+    document.getElementById("movie-details-container").innerHTML = detailsHtml
+}
+
+
+function clearPlaceholders () {
     document.getElementById("film-icon").style.visibility = "hidden"
     document.getElementById("icon-subtext").style.visibility = "hidden"
-    document.getElementById("results-container").innerHTML = html
-
-    movieArray = []
 }
