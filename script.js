6439575414bfe8333ea946fc9fc8e037739ca3d6
Joseph Kleinhenz <115609619+joeklein9@users.noreply.github.com>
2023-02-15 15:43:58 -0500

    watchlist buttons console log each movies imdbID

--- script.js
+++ script.js
@@ -1,20 +1,28 @@
-
 movieArray = []
 const searchBar = document.getElementById("search-bar")
 const searchButton = document.getElementById("search-button")
 const resultsContainer = document.getElementById("results-container")
+const watchListButton = document.getElementById("add-watchlist-button")
+
+
 
 searchButton.addEventListener("click", function () {
     fetch(`https://www.omdbapi.com/?s=${searchBar.value}&apikey=ec55d5fb`)
         .then(res => res.json())
         .then(data => {
-            console.log(data.Search)
+
             for (let i = 0; i < data.Search.length; i++) {
                 movieArray.push(data.Search[i])
             }
             renderResults()
+            document.querySelectorAll(".add-watchlist-button").forEach(item => {
+                item.addEventListener("click", event => { console.log(item.id) })
+            })
 
         })
+
+
+
 })
 
 
@@ -23,15 +31,26 @@ function renderResults() {
     for (let movie of movieArray) {
         html += `
             <h3>${movie.Title} (${movie.Year})</h3>
-            <button id = "add-watchlist-button">Add to watchlist</button>
+            <button class = "add-watchlist-button" id ="${movie.imdbID}" "data-id = "${movie.imdbID}">Add to watchlist</button>
             <img src="${movie.Poster}" class = "movie-posters">
+            <p> IMDB ID: ${movie.imdbID}</p>
+        
+  
             <hr />
             
         `
+
     }
+
     document.getElementById("film-icon").style.visibility = "hidden"
     document.getElementById("icon-subtext").style.visibility = "hidden"
     document.getElementById("results-container").innerHTML = html
 
+
     movieArray = []
+
 }
+
+
+
+
