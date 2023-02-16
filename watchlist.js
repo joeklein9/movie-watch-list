c629a601f14a7b6861415686b2e320ff249a31be
Joseph Kleinhenz <115609619+joeklein9@users.noreply.github.com>
2023-02-15 21:06:45 -0500

    finished for now

--- watchlist.js
+++ watchlist.js
@@ -1,13 +1,20 @@
 
 
+const watchListArray = []
+
 let fromStorage = JSON.parse(window.localStorage.getItem("addedMovieId"))
 
 console.log(fromStorage)
+
 
 fetch(`https://www.omdbapi.com/?i=${fromStorage}&apikey=ec55d5fb`)
     .then (response => response.json())
     .then (data => {
         console.log(data)
+
+        watchListArray.push(fromStorage)
+        console.log(watchListArray)
+        
         document.getElementById("results-container").innerHTML += `
         
         <h3>${data.Title} (${data.Year})</h3>
